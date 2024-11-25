4162122dcd288c85b8feb1d8a93c0b4f0298469a
Sentry2703 <adetunjiadewoye1@gmail.com>
2024-11-24 22:54:50 -0500

    Learning Composables

--- MatchCompose/app/src/main/java/com/android/matchcompose/MainActivity.kt
+++ MatchCompose/app/src/main/java/com/android/matchcompose/MainActivity.kt
@@ -3,12 +3,18 @@ package com.android.matchcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.android.matchcompose.ui.theme.MatchComposeTheme
 
@@ -16,31 +22,30 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MatchComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Greeting(name = "Tunji")
         }
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column (modifier = Modifier.fillMaxSize(0.99f).background(Color.Green),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly){
+        for (i in 0 until 4) {
+            Text(
+                text = "Hello $name!",
+                modifier = modifier
+            )
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MatchComposeTheme {
-        Greeting("Android")
+        Greeting("j")
     }
 }

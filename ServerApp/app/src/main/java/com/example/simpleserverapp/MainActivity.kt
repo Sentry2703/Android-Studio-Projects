fcb9b074423e8430476d8bd9af53fe5e77e08b77
Sentry2703 <adetunjiadewoye1@gmail.com>
2025-03-15 20:26:12 -0700

    Deleted Things

--- ServerApp/app/src/main/java/com/example/simpleserverapp/MainActivity.kt
+++ ServerApp/app/src/main/java/com/example/simpleserverapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SimpleServerAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Tunji",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello $name! What's going on?",
         modifier = modifier
     )
 }

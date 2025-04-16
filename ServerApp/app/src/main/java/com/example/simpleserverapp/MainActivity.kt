3c2e87845b74aa95e83fbeb2e252ec90e1a7c220
Sentry2703 <adetunjiadewoye1@gmail.com>
2025-04-16 14:25:14 -0400

    Modiifed MainActivity for Navigation Only

--- ServerApp/app/src/main/java/com/example/simpleserverapp/MainActivity.kt
+++ ServerApp/app/src/main/java/com/example/simpleserverapp/MainActivity.kt
@@ -6,50 +6,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresExtension
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.simpleserverapp.NetworkHelper.NetworkDiscover
-import com.example.simpleserverapp.ui.theme.SimpleServerAppTheme
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.unit.dp
-import com.example.simpleserverapp.sampledata.mockNsdServices
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.simpleserverapp.core.model.ServerInfo
+import com.example.simpleserverapp.core.network.NetworkDiscover
+import com.example.simpleserverapp.views.ui.theme.SimpleServerAppTheme
+import com.example.simpleserverapp.views.filetransfer.FileTransferScreen
+import com.example.simpleserverapp.views.start.ServerDiscoveryScreen
+import com.example.simpleserverapp.views.navigation.Screen
 
 class MainActivity : ComponentActivity() {
 
     private lateinit var nsdHelper: NetworkDiscover
 
+
     @RequiresExtension(extension = Build.VERSION_CODES.TIRAMISU, version = 7)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         nsdHelper = NetworkDiscover(this)
-
         setContent {
+            val navController = rememberNavController()
             val services = remember { mutableStateListOf<NsdServiceInfo>() }
+
             LaunchedEffect(Unit) {
                 nsdHelper.startDiscovery {
                     services.clear()
@@ -57,106 +41,42 @@ class MainActivity : ComponentActivity() {
                 }
             }
 
-            SimpleServerAppTheme {
-                ServerDiscoveryScreen(
-                    services =  mockNsdServices(),
-                    onRefresh = {
-//                        nsdHelper.startDiscovery {
-//                            services.clear()
-//                            services.addAll(it)
-//                        }
-                    }
-                )
-            }
-        }
-    }
-}
-
-@Composable
-fun ServerDiscoveryScreen (
-    services: List<NsdServiceInfo>,
-    onRefresh: () -> Unit,
-){
-    Box(modifier = Modifier.fillMaxSize()) {
-        Column(
-            modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp, vertical = 24.dp)
-        ) {
-            Text(
-                text = "Discovered Servers",
-                style = MaterialTheme.typography.headlineMedium,
-                modifier = Modifier.padding(bottom = 16.dp)
-            )
-
-            LazyColumn (
-                verticalArrangement = Arrangement.spacedBy(12.dp),
-                modifier = Modifier.fillMaxWidth()
+            NavHost(
+                navController = navController, startDestination = Screen.ServerDiscovery.route
             ) {
-                if (services.isEmpty()) {
-                    item {
-                        Text("No servers found.", style = MaterialTheme.typography.bodyMedium)
+                composable(Screen.ServerDiscovery.route) {
+                    SimpleServerAppTheme {
+                        ServerDiscoveryScreen(
+                            services =  services,
+                            onRefresh = {
+                                nsdHelper.startDiscovery {
+                                    services.clear()
+                                    services.addAll(it)
+                                }
+                            },
+                            onServerSelected = {server ->
+                                navController.navigate(Screen.FileTransfer.createRoute(server))
+                            }
+                        )
                     }
-                } else {
-                    items(services) { service ->
-                        ServerCard(service)
+                }
+
+                composable(
+                    route = Screen.FileTransfer.route,
+                    arguments = listOf(
+                        navArgument("serverName") { type = NavType.StringType},
+                        navArgument("ip") { type = NavType.StringType },
+                    )
+                ) { backStackEntry ->
+                    val serverName = backStackEntry.arguments?.getString("serverName") ?: ""
+                    val ip = backStackEntry.arguments?.getString("ip") ?: ""
+
+                    SimpleServerAppTheme {
+                        FileTransferScreen(ServerInfo(name = serverName, ip = ip))
                     }
                 }
             }
-        }
 
-        Row (
-            modifier = Modifier.align(Alignment.BottomEnd).padding(bottom = 12.dp)
-        ) {
-            FloatingActionButton(
-                onClick = {},
-                modifier = Modifier
-                    .padding(16.dp)
-            ) {
-                Icon(Icons.Default.PlayArrow, contentDescription = "Connect")
-            }
-
-            FloatingActionButton(
-                onClick = {},
-                modifier = Modifier
-                    .padding(16.dp)
-            ) {
-                Icon(Icons.Default.Refresh, contentDescription = "Refresh")
-            }
-        }
-    }
-}
-
-@Composable
-@Preview(showBackground = true)
-fun ServerScreen() {
-    ServerDiscoveryScreen(
-        services = mockNsdServices(), //services
-        onRefresh = {
-        }
-    )
-}
-
-@Composable
-fun ServerCard(service: NsdServiceInfo) {
-    Card(
-        modifier = Modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(16.dp),
-        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
-        onClick = {
-        }
-    ) {
-        Column (modifier = Modifier.padding(16.dp)) {
-            Text(
-                text = service.serviceName,
-                style = MaterialTheme.typography.titleMedium
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-            service.hostAddresses.forEach { address ->
-                Text(
-                    text = "IP: $address",
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.primary
-                )
-            }
         }
     }
 }
